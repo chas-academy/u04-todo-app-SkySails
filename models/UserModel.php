55f4b18f806190a1231b78ba32f30caf54795e33
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-10 21:20:46 +0100

    Add ability to register users

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,10 +1,22 @@
-<?php
+<?php declare (strict_types = 1);
 
 class UserModel extends Database
 {
-    public function getProfile($id)
+    public static function getProfile(string $id)
     {
         $data = self::query('SELECT * FROM users WHERE id = ?', array($id));
-        return $data;
+        return $data[0];
+    }
+
+    public static function registerUser(array $params)
+    {
+        if (isset($params)) {
+            extract($params);
+            $id = uniqid();
+            $pw_hash = password_hash($password, PASSWORD_BCRYPT);
+
+            self::query('INSERT INTO users (id, name, email, pw_hash) VALUES (?, ?, ?, ?)', array($id, $name, $email, $pw_hash));
+        }
+
     }
 }
