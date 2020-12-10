d1a542ae33d0ea1ce27ab267c9b02e1d4b1652c3
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-10 21:20:28 +0100

    Add better db logic

--- classes/Database.php
+++ classes/Database.php
@@ -9,8 +9,14 @@ class Database
     {
         if (!isset(self::$pdo)) {
             $ini = parse_ini_file("dbconfig.ini");
-            self::$pdo = new PDO("mysql:host={$ini["host"]};dbname={$ini["dbname"]};charset=utf8", $ini["user"], $ini["password"]);
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            try {
+                self::$pdo = new PDO("mysql:host={$ini["host"]};dbname={$ini["dbname"]};charset=utf8", $ini["user"], $ini["password"]);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo 'Connection failed: ' . $e->getMessage();
+            }
         }
 
         return self::$pdo;
