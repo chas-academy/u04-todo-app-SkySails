9773fa767b2db8c4793a3d9871eb1ede8210d187
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-11 15:11:51 +0100

    Update routes to new format

--- Routes.php
+++ Routes.php
@@ -1,48 +1,31 @@
 <?php
 
 Route::add('/', function () {
-    Index::CreateView("Index");
-});
-
-Route::add('/todos', function () {
-    Todos::CreateView("Todos");
+    Index::RenderView(["data" => "Test"]);
 });
 
 Route::add('/account', function () {
-    echo <<<HTML
-        <h1>Accounts</h1>
-HTML;
+    Account::RenderView();
 });
 
-Route::add('/account/:id', function ($id) {
-    $html = <<<HTML
-        <h1>Account</h1>
-HTML;
-    $account_data = UserModel::getProfile($id);
-    var_dump($account_data);
-    foreach ($account_data as $key => $value) {
-        echo $key . " => " . $value . "<br/>";
-    }
+// Route::add('/account/:id', function ($id) {
+//     Account::RenderView($id);
+// });
+
+Route::add('/register', function () {
+    Register::RenderView();
 });
 
 Route::add('/register', function () {
-    echo <<<HTML
-        <h1>Register</h1>
-        <form method="post" style="display: flex; flex-direction: column; align-items: flex-start;">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name">
-            <label for="name">Email</label>
-            <input type="email" name="email" id="email">
-            <label for="name">Password</label>
-            <input type="password" name="password" id="password">
-            <button>Register</button>
-        </form>
-HTML;
-});
-
-Route::add('/register', function () {
-    echo 'Hey! The form has been sent:<br/>';
     UserModel::registerUser($_POST);
+}, "post");
+
+Route::add('/login', function () {
+    Login::RenderView();
+});
+
+Route::add('/login', function () {
+    UserModel::signinUser($_POST);
 }, "post");
 
 Route::pathNotFound(function () {
