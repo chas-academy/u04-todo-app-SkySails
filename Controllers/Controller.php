c018cd123e82bacb9b40c93603366bb745e66234
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-11 15:13:00 +0100

    Add middleware static function

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -2,6 +2,17 @@
 
 class Controller extends DataBase
 {
+
+    public static function middleware()
+    {
+        session_start();
+        $signed_in = isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true;
+        if (!$signed_in) {
+            header("location: login");
+            exit;
+        }
+    }
+
     public static function CreateView($viewName)
     {
         require_once "./Views/$viewName.php";
