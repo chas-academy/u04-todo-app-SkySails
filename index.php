189c3505f5b8e83c037f6802146edf74cd944ef7
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-11 15:10:17 +0100

    Remove comment

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 spl_autoload_register(function ($class_name) {
     if (file_exists('./classes/' . $class_name . '.php')) {
         require_once './classes/' . $class_name . '.php';
@@ -8,7 +9,5 @@ spl_autoload_register(function ($class_name) {
         require_once './models/' . $class_name . '.php';
     }
 });
-
-// I could load the database here, although that would mean initialization even if not needed on tha page
 
 require_once "Routes.php";
