89ec10b99a1ba8eb43cb11040392681916e36a5b
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-14 21:24:48 +0100

    Add API routes

--- Routes.php
+++ Routes.php
@@ -1,19 +1,27 @@
 <?php
 
+//?###################
+//?  Base routes    ##
+//?###################
+
 Route::add('/', function () {
-    Index::RenderView(["data" => "Test"]);
+    Index::RenderView();
+});
+
+Route::add('/renderajax', function () {
+    Index::RenderView("template");
 });
 
 Route::add('/account', function () {
     Account::RenderView();
 });
 
-// Route::add('/account/:id', function ($id) {
-//     Account::RenderView($id);
-// });
+//?###################
+//?  Auth routes    ##
+//?###################
 
 Route::add('/register', function () {
-    Register::RenderView();
+    Auth::RenderView("register");
 });
 
 Route::add('/register', function () {
@@ -21,12 +29,65 @@ Route::add('/register', function () {
 }, "post");
 
 Route::add('/login', function () {
-    Login::RenderView();
+    Auth::RenderView("login");
 });
 
 Route::add('/login', function () {
     UserModel::signinUser($_POST);
 }, "post");
+
+//?###################
+//?   API routes    ##
+//?###################
+
+// Get all lists
+Route::add('/api/lists', function () {
+    session_start();
+    echo "Get all lists" . $_SESSION["id"];
+});
+
+// Create list
+Route::add('/api/lists', function () {
+    TodoModel::addList($_POST["name"]);
+}, "post");
+
+// Get list with id
+Route::add('/api/lists/:id', function ($list_id) {
+    echo "Get list with id: " . $list_id;
+});
+
+// Delete list with id
+Route::add('/api/lists/:id', function ($list_id) {
+    TodoModel::deleteList($list_id);
+}, "delete");
+
+// Get items
+Route::add('/api/items', function () {
+    echo "Get all items";
+});
+
+// Add item
+Route::add('/api/items', function () {
+    // echo "Add item with title: " . $_POST["title"];
+    TodoModel::addTodo($_POST);
+}, "post");
+
+// Get item with id
+Route::add('/api/items/:id', function ($item_id) {
+    echo "Get item with id: " . $item_id;
+});
+
+// Update item with id
+Route::add('/api/items/:id', function ($item_id) {
+    parse_str(file_get_contents('php://input'), $_PATCH);
+
+    TodoModel::updateTodo(intval($item_id), $_PATCH);
+}, "patch");
+
+// Delete item with id
+Route::add('/api/items/:id', function ($item_id) {
+    TodoModel::deleteTodo($item_id);
+}, "delete");
 
 Route::pathNotFound(function () {
     echo <<<HTML
