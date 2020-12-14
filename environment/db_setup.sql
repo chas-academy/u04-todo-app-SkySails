023e0240ae39e43dd5e4a50604ddc029937f2d2b
Malte <46646495+SkySails@users.noreply.github.com>
2020-12-14 21:32:04 +0100

    Add database structure as SQL file

---
+++ environment/db_setup.sql
@@ -0,0 +1,68 @@
+/*
+
+ Source Server         : todoapp.se
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : localhost:3306
+ Source Schema         : todoapp
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 14/12/2020 21:28:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for lists
+-- ----------------------------
+DROP TABLE IF EXISTS `lists`;
+CREATE TABLE `lists`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `UserID_list`(`created_by`) USING BTREE,
+  CONSTRAINT `UserID_list` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for todos
+-- ----------------------------
+DROP TABLE IF EXISTS `todos`;
+CREATE TABLE `todos`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'The unique id of the todo',
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The title of the todo',
+  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'The content of the todo',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'The status of the todo',
+  `created_by` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The id of the user that submitted the todo',
+  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Then date/time at which the todo was created',
+  `list_id` int(11) NULL DEFAULT NULL COMMENT 'The id for the list in which the dodo is listed',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `UserID`(`created_by`) USING BTREE,
+  INDEX `ListId`(`list_id`) USING BTREE,
+  CONSTRAINT `UserID` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `ListId` FOREIGN KEY (`list_id`) REFERENCES `lists` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pw_hash` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`, `email`) USING BTREE,
+  UNIQUE INDEX `UserEmail`(`email`) USING BTREE COMMENT 'A user\'s email, should be unique',
+  INDEX `id`(`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
