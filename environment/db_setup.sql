3b7d9470fa2d391d556e46a267d7aa39ae0a98d7
Malte <46646495+SkySails@users.noreply.github.com>
2021-01-07 17:21:45 +0100

    Add comments

--- environment/db_setup.sql
+++ environment/db_setup.sql
@@ -6,10 +6,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `lists`;
 CREATE TABLE `lists`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'A users email should be unique',
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'A users email should be unique',
+  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The time at which the list was created',
+  `created_by` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'The id of the user that submitted the list',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `UserID_list`(`created_by`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -36,14 +36,14 @@ CREATE TABLE `todos`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `pw_hash` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The unique id of the user',
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The name of the user',
+  `email` varchar(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The email of the user',
+  `pw_hash` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'The password hash and salt separated concatenated',
+  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The time at which the user was created',
+  `modified_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'The time at which the user was updated',
   PRIMARY KEY (`id`, `email`) USING BTREE,
-  UNIQUE INDEX `UserEmail`(`email`) USING BTREE COMMENT 'A users email, should be unique',
+  UNIQUE INDEX `UserEmail`(`email`) USING BTREE COMMENT 'A users email should be unique',
   INDEX `id`(`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
